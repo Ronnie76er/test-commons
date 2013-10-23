fbc80b6f2dedd77fb2aa2228bae88eb9c8c303df
Ron Alleva <ron@konciergemd.com>
2013-10-23 10:50:08 -0400

    Initial commit

---
+++ test-api/build.sbt
@@ -0,0 +1,16 @@
+import sbtrelease._
+import ReleaseStateTransformations._
+
+name := "test-api"
+version := "1.0"
+
+
+
+libraryDependencies ++= Seq(
+     "joda-time" % "joda-time" % "2.3"
+)
+
+publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/Projects/tools")))
+
+releaseSettings
+
---
+++ test-api/src/main/java/com/konciergemd/import/binders/TestBinder.java
@@ -0,0 +1,21 @@
+package com.konciergemd.import.binders;
+
+// import binders.serializers.JsonDateDeserializer;
+// import binders.serializers.JsonLocalDateSerializer;
+// import common.DateTimeUtil;
+// import importers.binders.BaseImportBinder;
+// import models.billing.ImportedBillingEntry;
+// import models.billing.ImportedDiagnosisCode;
+// import org.apache.commons.collections.CollectionUtils;
+// import org.codehaus.jackson.map.annotate.JsonDeserialize;
+// import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestBinder  {
+
+    public LocalDate serviceDate;
+
+}
